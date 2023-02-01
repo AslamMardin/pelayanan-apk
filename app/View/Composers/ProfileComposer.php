4440f26b2971140ed0fb46ddd1ae1793bc7fc772
aslam mardin <aslam99mardin@gmail.com>
2023-02-01 14:32:53 +0700

    slalah

---
+++ app/View/Composers/ProfileComposer.php
@@ -0,0 +1,22 @@
+<?php
+ 
+namespace App\View\Composers;
+ 
+
+use Illuminate\View\View;
+ 
+class ProfileComposer
+{
+ 
+ 
+    /**
+     * Bind data to the view.
+     *
+     * @param  \Illuminate\View\View  $view
+     * @return void
+     */
+    public function compose(View $view)
+    {
+        $view->with('count', 'accant');
+    }
+}
